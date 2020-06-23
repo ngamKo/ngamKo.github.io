d6b5a94b6d5d1c327deba5765365b2fdb133086f
ngamKo <67314787+ngamKo@users.noreply.github.com>
2020-06-23 23:32:35 +0900

    Update kadai10.m

--- kadai10.m
+++ kadai10.m
@@ -1,36 +1,11 @@
 % 課題10 画像のエッジ抽出 
-% 次のプログラムを参考にして，エッジ抽出を体験せよ．
-% 各自，Lenna以外の画像を用いよ． 
-% 例
 
-ORG = imread('Lenna.jpg'); % 原画像の入力
-ORG = rgb2gray(ORG); %カラーからグレイへの変換
-imagesc(ORG); colormap('gray'); colorbar;% 画像表示
-pause; % 一時停止
+I = imread('nk.png'); % 原画像の入力
+I = rgb2gray(I); %カラーからグレイへの変換
+imagesc(I); colormap('gray'); colorbar;% 画像表示
 
-IMG = edge(ORG,'prewitt'); % エッジ抽出（プレウィット法）
-imagesc(IMG); colormap('gray'); colorbar;% 画像表示
-pause; % 一時停止
-
-IMG = edge(ORG,'sobel'); % エッジ抽出（ソベル法）
-imagesc(IMG); colormap('gray'); colorbar;% 画像表示
-pause; % 一時停止
-
-IMG = edge(ORG,'canny'); % エッジ抽出（キャニー法）
-imagesc(IMG); colormap('gray'); colorbar;% 画像表示
-pause; % 一時停止
-
-
-
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+BW1 = edge(I,'sobel');
+BW2 = edge(I,'canny');
+figure;
+imshowpair(BW1,BW2,'montage')
+title('kadai10');
